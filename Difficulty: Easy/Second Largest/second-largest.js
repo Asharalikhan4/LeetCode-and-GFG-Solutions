1202c80f1e06463141bd808d3d4075d79e2cde4b
ASHAR ALI KHAN <87232958+Asharalikhan4@users.noreply.github.com>
2026-04-21 20:56:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.js
+++ Difficulty: Easy/Second Largest/second-largest.js
@@ -1,12 +1,13 @@
 class Solution {
     getSecondLargest(arr) {
         // code here
-        let largest = 0, secondLargest = 0;
-        for (let i = 0; i < arr.length; i++) {
-            if(arr[i] > largest) {
+        let n = arr.length;
+        let largest = -1, secondLargest = -1;
+        for (let i = 0; i < n; i++) {
+            if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
-            } else if(largest > arr[i] && arr[i] > secondLargest) {
+            } else if (arr[i] < largest && arr[i] > secondLargest) {
                 secondLargest = arr[i];
             }
         }
